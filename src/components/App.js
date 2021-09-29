685b31f4e93d0985edbaa4189e42ec9e07742dfa
MrSayyod <ssayyod@gmail.com>
2021-09-29 17:24:57 -0600

    Implement the App component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import BooksList from '../containers/BooksList'
+import BooksForm from '../containers/BooksForm'
+
+const App = () => {
+  return (
+    <div className="App">
+      <BooksList />
+      <BooksForm />
+    </div>
+  )
+}
+
+export default App
