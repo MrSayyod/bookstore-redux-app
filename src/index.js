52d0ca31fbb75d68a90a83d2870b08705b1616fc
MrSayyod <ssayyod@gmail.com>
2021-09-29 20:55:40 -0600

    Fix eslint errors

--- src/index.js
+++ src/index.js
@@ -1,37 +1,37 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./components/App";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
-import finalReducer from "./reducers/index";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from './components/App';
+import finalReducer from './reducers/index';
 
 const initial = {
   books: [
     {
       id: Math.round(Math.random() * 100) + 1,
-      title: "Book_1",
-      category: "Learning",
+      title: 'Book_1',
+      category: 'Learning',
     },
     {
       id: Math.round(Math.random() * 100) + 1,
-      title: "Book_2",
-      category: "Action",
+      title: 'Book_2',
+      category: 'Action',
     },
     {
       id: Math.round(Math.random() * 100) + 1,
-      title: "Book_3",
-      category: "Sci-fi",
+      title: 'Book_3',
+      category: 'Sci-fi',
     },
     {
       id: Math.round(Math.random() * 100) + 1,
-      title: "Book_4",
-      category: "Kids",
+      title: 'Book_4',
+      category: 'Kids',
     },
     {
       id: Math.round(Math.random() * 100) + 1,
-      title: "Book_5",
-      category: "History",
+      title: 'Book_5',
+      category: 'History',
     },
   ],
 };
@@ -44,7 +44,7 @@ ReactDOM.render(
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,16 +1,14 @@
-import { act } from "react-dom/cjs/react-dom-test-utils.production.min";
-import { CREATE_BOOK } from "../actions/index";
-import { REMOVE_BOOK } from "../actions/index";
+import { CREATE_BOOK, REMOVE_BOOK } from '../actions/index';
 
 const start = {
   books: [
-    { id: "1", title: "1984", category: "Sci-fi" },
-    { id: "2", title: "To kill a mockingbird", category: "Horror" },
-    { id: "3", title: "Robinson Crusoe", category: "History" },
+    { id: '1', title: '1984', category: 'Sci-fi' },
+    { id: '2', title: 'To kill a mockingbird', category: 'Horror' },
+    { id: '3', title: 'Robinson Crusoe', category: 'History' },
   ],
 };
 
-const booksReducer = (state = initial, action) => {
+const booksReducer = (state = start, action) => {
   switch (action.type) {
     case CREATE_BOOK:
       return [
@@ -21,17 +19,14 @@ const booksReducer = (state = initial, action) => {
           category: action.book.category,
         },
       ];
-      break;
     case REMOVE_BOOK:
       return [
         state.slice(0, action.index),
         state.slice(action.index + 1, state.length),
       ];
-      break;
     default:
       return state;
   }
 };
 
-
-export default booksReducer
+export default booksReducer;
