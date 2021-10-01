408f5aa0d641fe43edf597eedea1dd39e8a7bca6
MrSayyod <ssayyod@gmail.com>
2021-10-01 17:19:09 -0600

    Implement CategoryFilter component

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,3 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+const CATEGORIES = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi']
+
+const CategoryFilter = ({ handleFilterChange }) => (
+  <select name="category" onChange={handleFilterChange} >
+    <option value="ALL">ALL</option>
+    {
+      CATEGORIES.map((item) => (
+        <option key={item} value={item}>{item}</option>
+      ))
+    }
+  </select>
+)
