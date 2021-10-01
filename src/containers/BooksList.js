bf582808c643b9af8256c51bdf5d244ba6a5adb5
MrSayyod <ssayyod@gmail.com>
2021-10-01 17:26:18 -0600

    Import CategoryFilter component

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Book from '../components/Book';
 import { actionRemoveBook, actionChangeFilter } from '../actions/index';
+import { CategoryFilter } from '../components/CategoryFilter';
 
 const BooksList = ({ books, removeBook }) => {
   const handleRemoveBook = (book) => {
