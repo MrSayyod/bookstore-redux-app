0c14e1e47cece18798ed6085a6f58830d3ff20a2
MrSayyod <ssayyod@gmail.com>
2021-10-01 17:37:20 -0600

    Delete comments

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,13 +1,5 @@
 import { CREATE_BOOK, REMOVE_BOOK } from '../actions/index';
 
-// const start = {
-//   books: [
-//     { id: '1', title: '1984', category: 'Sci-fi' },
-//     { id: '2', title: 'To kill a mockingbird', category: 'Horror' },
-//     { id: '3', title: 'Robinson Crusoe', category: 'History' },
-//   ],
-// };
-
 const booksReducer = (state = [], action) => {
   switch (action.type) {
     case CREATE_BOOK:
@@ -20,10 +12,6 @@ const booksReducer = (state = [], action) => {
         },
       ];
     case REMOVE_BOOK:
-      // return [
-      //   state.slice(0, action.index),
-      //   state.slice(action.index + 1, state.length),
-      // ];
       return state.filter((item) => item.id !== action.book.id);
     default:
       return state;
