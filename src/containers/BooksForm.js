587fbb367abf0a99281d766f0b100f68a4819040
MrSayyod <ssayyod@gmail.com>
2021-10-02 19:36:27 -0600

    Refactor form for adding books

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { actionCreateBook } from '../actions/index';
 import { CATEGORIES } from '../components/CategoryFilter';
-import '../styles/BooksForm.css'
+import '../styles/BooksForm.css';
 
 const BooksForm = ({ createBook }) => {
   const [state, setState] = useState({ title: '', category: 'Action' });
@@ -12,6 +12,7 @@ const BooksForm = ({ createBook }) => {
   const handleChange = ({ target: { name, value } }) => {
     setState({ ...state, [name]: value });
   };
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const { title, category } = state;
@@ -30,7 +31,24 @@ const BooksForm = ({ createBook }) => {
 
   return (
     <>
-      <form className="form" onSubmit={handleSubmit}>
+      <div className="bookFormSection">
+        <div className="addNewBook">Add new book</div>
+        <form className="form" onSubmit={handleSubmit}>
+          <input type="text" className="input-title" name="title" onChange={handleChange} value={state.title || ''} placeholder="Book title" />
+          <select className="category" name="category" onChange={handleChange} placeholder="Category" value={state.category}>
+            {
+              CATEGORIES.map((item) => (
+                <option key={item} value={item} className="categoryText">{item}</option>
+              ))
+            }
+          </select>
+          <button className="addBookBTN" type="submit">
+            <span className="addBookText">Add book</span>
+          </button>
+        </form>
+      </div>
+
+      {/* <form className="form" onSubmit={handleSubmit}>
         <h3>Add book</h3>
         <div className="title-div">
           <label htmlFor="book-title">
@@ -40,7 +58,7 @@ const BooksForm = ({ createBook }) => {
               id="book-title"
               name="title"
               onChange={handleChange}
-              value={state.title || ''}
+              value={state.title || ""}
             />
           </label>
         </div>
@@ -62,8 +80,8 @@ const BooksForm = ({ createBook }) => {
           </label>
         </div>
         <button type="submit">Submit</button>
-        <h5>{error ? 'Enter required fields' : ''}</h5>
-      </form>
+        <h5>{error ? "Enter required fields" : ""}</h5>
+      </form> */}
     </>
   );
 };
